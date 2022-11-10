ca63f722150a54f540277a58565f804256f22b1e
Abhi29 <Abhishe Tiwari>
2022-11-10 13:52:03 +0530

    [feat] Constant files added.

---
+++ src/constants/paths.constant.ts
@@ -0,0 +1,23 @@
+export const queryParam = {
+  TEAM: 'team',
+  INDIVIDUAL: 'individual',
+};
+
+export const paths = {
+  HOME: '/home',
+  PRODUCT: '/product',
+  SPECIAL_REQUEST: '/special_request',
+  PRODUCT_LISTING: '/product-list',
+  NOT_FOUND: '/not-found',
+  CHECKOUT: '/checkout',
+  MY_ACCOUNT: '/my-account',
+  SIGN_UP: '/sign-up',
+  THANK_YOU: '/thank-you',
+  CART: '/cart.html',
+  BRAND: '/brands.html',
+  WISHLIST: '/wishlist',
+  WRITEREVIEW: '/writereview',
+  REQUEST_CONSULTATION: '/request_consultation',
+  CUSTOMIZE_LOGO: '/customize',
+  PRODUCT_COMPARE: '/compare',
+};
