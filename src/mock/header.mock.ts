c008ac80bc942a21293a7488a90a4d20852acf3e
Abhi29 <Abhishe Tiwari>
2022-11-10 15:06:09 +0530

    [feat] mock files added.

---
+++ src/mock/header.mock.ts
@@ -0,0 +1,9 @@
+export const headerInfo = {
+  brand: {
+    label: 'Corporate Gear',
+    logo: {
+      url: 'https://www.corporategear.com/images/logo.svg',
+      alt: 'Corporate Gear',
+    },
+  },
+};
