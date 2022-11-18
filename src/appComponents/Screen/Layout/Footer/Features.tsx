9b7c851c5414b3cb7ec33d19fa986bb0b75646b1
Abhi29 <Abhishe Tiwari>
2022-11-18 12:30:09 +0530

    [feat] dummy feature content added.

--- src/appComponents/Screen/Layout/Footer/Features.tsx
+++ src/appComponents/Screen/Layout/Footer/Features.tsx
@@ -30,7 +30,52 @@ const Features: React.FC<_props> = ({ features }) => {
     );
   }
 
-  return <div dangerouslySetInnerHTML={{ __html: features }}></div>;
+  // return <div dangerouslySetInnerHTML={{ __html: features }}></div>;
+  if (storeLayout === _Store.type1) {
+    return (
+      <div className="bg-secondary">
+        <div className="container mx-auto md:py-5 text-center">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full md:w-auto inline-block px-8 py-5 md:py-0 border-b md:border-b-0 md:border-r last:border-r-0 last:border-b-0 border-slate-800">
+              <div className="w-full md:w-auto flex flex-wrap justify-center items-center">
+                <span className="material-icons text-4xl" role="heading">
+                  local_shipping
+                </span>
+                <div className="leading-5 ml-2 text-left text-default-text font-default-text">
+                  <div className="font-semibold">Free Shipping</div>
+                  <div className="">To One Location</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full md:w-auto inline-block px-8 py-5 md:py-0 border-b md:border-b-0 md:border-r last:border-r-0 last:border-b-0 border-slate-800">
+              <div className="w-full md:w-auto flex flex-wrap justify-center items-center">
+                <span className="material-icons text-4xl" role="heading">
+                  style
+                </span>
+                <div className="leading-5 ml-2 text-left text-default-text font-default-text">
+                  <div className="font-semibold">1st Logo Free</div>
+                  <div className="">Up To 10,000 Stitches</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full md:w-auto inline-block px-8 py-5 md:py-0 border-b md:border-b-0 md:border-r last:border-r-0 last:border-b-0 border-slate-800">
+              <div className="w-full md:w-auto flex flex-wrap justify-center items-center">
+                <span className="material-icons text-4xl" role="heading">
+                  verified
+                </span>
+                <div className="leading-5 ml-2 text-left text-default-text font-default-text">
+                  <div className="font-semibold">Free Proof</div>
+                  <div className="">On All Orders</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <></>;
 };
 
 export default Features;
