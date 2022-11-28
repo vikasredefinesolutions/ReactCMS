09625a8a1bf3ccdc609516f2ab12c60931e7cd0d
Abhi29 <Abhishe Tiwari>
2022-11-28 11:04:13 +0530

    [fix] missing "seo" var. Added dummy value for that.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,41 +4,47 @@ import { getServerSideProps } from 'pages/[slug]/getServerSideProps';
 export default function Search(props: any) {
   const { pageType, pageData, slug } = props;
 
-    let page = <>Loading ...</>;
-    if (pageType && pageData) {
-        if(pageType === 'topic')
-        {
-            const props = {
-                pageData: pageData,
-                pageType: pageType,
-                slug: slug,
-            }
-            page = <>
-                <Head>
-                    <title>{pageData?.seTitle}</title>
-                    <meta
-                        name="description"
-                        content={pageData?.seDescription}
-                        key="desc"
-                    />
-                    <meta name="keywords" content={pageData?.seKeyWords} />
-                </Head>
-                <Home props={props} />
-            </>
-        }
-
+  let page = <>Loading ...</>;
+  if (pageType && pageData) {
+    const seo = {
+      seTitle: 'TITLE HERE',
+      seDescription: 'DESCRIPTION',
+      seKeyWords: 'PAGE',
+    };
+    if (pageType === 'topic') {
+      const props = {
+        pageData: pageData,
+        pageType: pageType,
+        slug: slug,
+      };
       page = (
         <>
           <Head>
-            <title>{seo?.seTitle}</title>
-            <meta name="description" content={seo?.seDescription} key="desc" />
-            <meta name="keywords" content={seo?.seKeyWords} />
+            <title>{pageData?.seTitle}</title>
+            <meta
+              name="description"
+              content={pageData?.seDescription}
+              key="desc"
+            />
+            <meta name="keywords" content={pageData?.seKeyWords} />
           </Head>
-          <Home />
+          <Home props={props} />
         </>
       );
     }
-  
+
+    page = (
+      <>
+        <Head>
+          <title>{seo?.seTitle}</title>
+          <meta name="description" content={seo?.seDescription} key="desc" />
+          <meta name="keywords" content={seo?.seKeyWords} />
+        </Head>
+        <Home />
+      </>
+    );
+  }
+
   return <>{page}</>;
 }
 
