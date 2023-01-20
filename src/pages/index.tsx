bc0e880871960c1c7932573cc202e20ec3ecfa8d
vikasiweb <92905791+vikasiweb@users.noreply.github.com>
2023-01-20 12:04:46 +0530

    Add latest code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import SeoHead from 'appComponents/reUsable/SeoHead';
 import { getServerSideProps } from 'Components/Slug/getServerSideProps';
 import { cLog } from 'helpers/global.console';
 import { NextPage } from 'next';
-import Home from 'pages/home';
+import Home from 'pages/Home';
 
 const TopicHome: NextPage<_SlugServerSideProps | _SlugServerSide_WentWrong> = (
   props,
