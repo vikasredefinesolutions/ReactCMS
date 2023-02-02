488e7849055a68c894388f44a0678fcb91f9739d
vikasiweb <92905791+vikasiweb@users.noreply.github.com>
2023-02-02 13:25:12 +0530

    punchout file update

--- src/pages/punchout/2go/start/index.tsx
+++ src/pages/punchout/2go/start/index.tsx
@@ -1,35 +1,40 @@
 import { PunchoutPostApi } from '@services/punchout.service';
 
-const Puchout = ({req,res,punchout} : any) => {
-
-    return (
-        <>
-            <h1>Request Stream</h1>
-            <span>{JSON.stringify(req?.data)}</span>
-            <h1>Response Stream</h1>
-            <span>{JSON.stringify(res?.data)}</span>
-            <h1>Punchout Api</h1>
-            <span>{JSON.stringify(punchout)}</span>
-
-        </>
-    )
-}
+const Puchout = ({ req, res, punchout }: any) => {
+  return (
+    <>
+      <h1>Request Stream</h1>
+      <span>{JSON.stringify(req?.data)}</span>
+      <h1>Response Stream</h1>
+      <span>{JSON.stringify(res?.data)}</span>
+      <h1>Punchout Api</h1>
+      <span>{JSON.stringify(punchout)}</span>
+    </>
+  );
+};
 
 export default Puchout;
 
-export const getServerSideProps   = async (context : any ) => {
-    const res = await PunchoutPostApi();
-    return {
-        props : {
-            req : {
-            data : {body : {...context.req.body},
-            headers: {...context.req.headers}}
-            },
-            res : {
-                data : {body : {...context.res.body},
-                headers: {...context.res.headers}}
-            },
-            punchout : res
-        }
-    }
-}
+export const getServerSideProps = async (context: any) => {
+  const res = await PunchoutPostApi();
+  console.log(context);
+  return {
+    props: {
+      req: {
+        data: {
+          body: { ...context.req.body },
+          headers: { ...context.req.headers },
+        },
+        params: { ...context.req?.params },
+        returnUrl: { ...context.req?.return_url },
+      },
+      res: {
+        data: {
+          body: { ...context.res.body },
+          headers: { ...context.res.headers },
+        },
+      },
+      punchout: res,
+    },
+  };
+};
